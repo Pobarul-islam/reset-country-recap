0fa3f337f59d581b5392c95558b316a3370c1756
Pobarul-islam <pobarul3@gmail.com>
2022-03-15 20:00:05 +0600

    reset country reacap

---
+++ counries.js
@@ -0,0 +1,57 @@
+const loadCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
+
+const displayCountries = (countries) => {
+  console.log(countries);
+  const CountriesHTML = countries.map((country) => getCountryHTML(country));
+  // console.log(allCountriesHTML[0])
+  const container = document.getElementById("countries");
+  container.innerHTML = CountriesHTML.join(" ");
+};
+
+// option 2
+const getCountryHTML = (country) => {
+  const { name, flags,area,region } = country;
+  return `
+
+        <div  class ="country">
+        <h2>${name.common}</h2>
+        <p>Area: ${area}</p>
+        <p>Region: ${region}</p>
+        <img src = "${flags.png}">
+        
+        </div>
+    
+    `;
+};
+
+// option 1
+
+//   const { name, flags } = country;
+//   return `
+
+//         <div  class ="country">
+//         <h2>${name.common}</h2>
+//         <img src = "${flags.png}">
+        
+//         </div>
+    
+//     `;
+// };
+
+// Original
+
+// const getCountryHTML = country => {
+//     return `
+//         <div  class ="country">
+//         <h2>${country.name.common}</h2>
+//         <img src = "${country.flags.png}">
+
+//         </div>
+
+//     `
+// }
+loadCountries();
